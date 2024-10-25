b9be10eb0f06ff626d96df1c0768de1f7cae9d36
nafilafiryal <nafilahfiryal05@gmail.com>
2024-10-25 08:21:51 +0700

    uts

--- uts.java
+++ uts.java
@@ -7,7 +7,10 @@ public class uts {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int harga;
+        int diskon;
         System.out.println("Masukkan harga: ");
         harga = input.nextInt();
+        System.out.println("Masukkan diskon: ");
+        diskon = input.nextInt();
     }
 }
