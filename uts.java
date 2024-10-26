5b44387e9a0744cde424f77270820095a1e09970
nafilafiryal <nafilahfiryal05@gmail.com>
2024-10-26 09:22:52 +0700

    uts

--- uts.java
+++ uts.java
@@ -6,10 +6,10 @@ public class uts {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean completeFile;
+        boolean completeFile, passesTestSelection, passesDocumentSelection;
         int numTakers;
-        String prospectiveEmployee;
-        double academicScore, englishScore, nationalismScore, averageScore;
+        String prospectiveEmployee, healthPhysical, healthMental;
+        double academicScore, englishScore, nationalismScore, averageScore, gpa;
         System.out.print("Insert name prospective employee: ");
         prospectiveEmployee = input.next();
         System.out.print("Enter the number of test takers: ");
@@ -27,20 +27,25 @@ public class uts {
                 if (nationalismScore >= 0 && nationalismScore <= 100) {
                     
                 } else {System.out.println("Invalid score! Please enter a score between 0 and 100.");}
-                
-            } else {System.out.println("Invalid score! Please enter a score between 0 and 100.");}
-            
+            } else {System.out.println("Invalid score! Please enter a score between 0 and 100.");}   
         } else {System.out.println("Invalid score! Please enter a score between 0 and 100.");}
         averageScore = (academicScore + englishScore + nationalismScore) / 3;
         System.out.println("Average Score Test: " + averageScore);
         System.out.println("Test Selection Completed!");
 
-        System.out.print("Complete File? (True / False): ");
-        completeFile = input.nextBoolean();
-        if (completeFile == true) {
-            System.out.println("Selection file is complete!");
-        } else {System.out.println("Incomplete selection file!");}
-
-
+        passesTestSelection = (averageScore >= 78) && (academicScore >= 75) && (englishScore >= 75) && (nationalismScore >= 75);
+        if (passesTestSelection) {
+            System.out.println("Test Taker " + numTakers + " " + "PASS the test selection. Proceed to file selection.");
+            System.out.print("Enter IPK: ");
+            gpa = input.nextDouble();
+            System.out.print("Is the test taker physically fit? (yes/no): ");
+            healthPhysical = input.next();
+            System.out.print("Is the test taker spiritually healthy? (yes/no): ");
+            healthMental = input.next();
+            passesDocumentSelection = (gpa >= 3.25 && gpa <= 4.00) && healthPhysical.equalsIgnoreCase("yes") && healthMental.equalsIgnoreCase("yes");
+            if (passesDocumentSelection) {
+                System.out.println("Test taker " + numTakers + " " + "Accepted." );
+            } else {System.out.println("Test Taker " + numTakers + " " + "Not Accepted (he completeness of the file does not meet the requirements).");}
+        } else {System.out.println("Test Taker " + numTakers + " " + "DO NOT PASS the test selection.");}
     }
 }
