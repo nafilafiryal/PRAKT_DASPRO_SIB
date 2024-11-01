9a2a74420eca0778f9f838f63ed424be591017f1
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-01 19:24:57 +0700

    Lab 4: Calculating Leave Entitlement Using DO-WHILE

---
+++ DoWhileLeaveEntitlement17.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class DoWhileLeaveEntitlement17 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int leaveEntitlement, numLeave;
+        String confirmation;
+
+        System.out.print("Input the number of leave entitlement: ");
+            leaveEntitlement = input.nextInt();
+
+        do {
+            System.out.print("Do you want to take a leave (y/n)? ");
+            confirmation = input.next();
+            if (confirmation.equalsIgnoreCase("y")) {
+                System.out.print("How many day(s)? ");
+                numLeave = input.nextInt();
+
+                if (numLeave <= leaveEntitlement) {
+                    leaveEntitlement -= numLeave;
+                    System.out.println("Remaining leave entitlement: " + leaveEntitlement);
+                    
+                } else {
+                    System.out.println("You dont have enough leave entitlement");
+                    break;
+                }
+            }
+        } while (leaveEntitlement > 0);
+    }
+}
