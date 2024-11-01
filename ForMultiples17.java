61554714e1acce8ae277a23dec19be0be6f9d813
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-01 11:12:32 +0700

    Lab 1 Question no 3

--- ForMultiples17.java
+++ ForMultiples17.java
@@ -5,6 +5,8 @@ public class ForMultiples17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int multiple, sum = 0, counter = 0;
+        double average = 0.0;
+        
 
         System.out.print("Input the multiple: ");
         multiple = input.nextInt();
@@ -13,9 +15,14 @@ public class ForMultiples17 {
             if (i % multiple == 0) {
                 sum = sum + i;
                 counter++;
+                
             }
-        } System.out.printf("There are %d number that are multiple of %d in range 1 to 50.\n", counter, multiple);
+            
+        } 
+        average = (double) sum / counter;
+        System.out.printf("There are %d number that are multiple of %d in range 1 to 50.\n", counter, multiple);
         System.out.printf("The sum of all multiples of %d in range 1 to 50 is %d.\n", multiple, sum);
+        System.out.printf("The average of all multiples of %d in range 1 to 50 is %.2f\n", multiple, average);
 
     }
 }
