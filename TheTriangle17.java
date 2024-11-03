cbfbf621ed5ba72339e36cad55752bf61997e19a
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-03 15:45:56 +0700

    Jobsheet 7 Assignment no 1

--- TheTriangle17.java
+++ TheTriangle17.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 public class TheTriangle17 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int numInput, i = 0;
+        int numInput, i;
         String s = "";
 
         System.out.print("Input some number: ");
         numInput = input.nextInt();
 
-        for ( i = 0; i < numInput; i++) {
-            s += " *";
-            System.out.println(s);
+        for ( i = numInput; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println("");
         }
 
     }
