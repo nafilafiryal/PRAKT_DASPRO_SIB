4070fdc874576b63d6d2d88f059d01c5fa0e4995
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-07 23:40:14 +0700

    Looping 2 Assignment 1 b

--- assigment1Aloop2.java
+++ assigment1Aloop2.java
@@ -4,9 +4,12 @@ public class assigment1Aloop2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        for (int r = 1; r < 11; r++) {
-            for (int c = r; c < 11; c++) {
-                System.out.print("* ");
+        for (int r = 1; r <= 10; r++) {
+            for (int s = 1; s <= 10 - r; s++) {
+                System.out.print("  ");
+            }
+            for (int c = 1; c <= r; c++) {
+                System.out.print(" *");
             }
             System.out.println();
         }
