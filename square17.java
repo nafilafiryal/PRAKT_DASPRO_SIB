4e43ca4dd08d29bfe5cc91b086e68ad738bcb6e5
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-08 08:45:12 +0700

    Jobsheet 8. Experiment 2.

--- square17.java
+++ square17.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public class star17 {
+public class square17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter value N: ");
         int N = sc.nextInt();
-
         for (int i = 1; i <= N; i++) {
-            System.out.print("*");
-            
+            for (int j = 1; j <= N; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
