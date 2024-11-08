0b0d5c834589a8081738395f1bdd04f8067ba4d2
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-08 08:14:28 +0700

    Jobsheet 8. experiment 1

--- assigment1Aloop2.java
+++ assigment1Aloop2.java
@@ -4,14 +4,12 @@ public class assigment1Aloop2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        for (int r = 1; r <= 10; r++) {
-            for (int s = 1; s <= 10 - r; s++) {
-                System.out.print("  ");
+        for (char r = 'a'; r <= 'z'; r++) {
+            for (int s = 8; s < s - 2 ; s--) {
+                System.out.print(" ");
             }
-            for (int c = 1; c <= r; c++) {
-                System.out.print(" *");
-            }
+            System.out.print(r);
             System.out.println();
-        }
+        } 
     }
 }
