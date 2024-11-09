c2fb6fa565b7606b6557df7977f5f4ce5b690383
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-09 20:55:14 +0700

    Loop 2. Assignment no 2.

--- assigment1Aloop2.java
+++ assigment1Aloop2.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class assigment1Aloop2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int row = 5;
+        char letter = 'a';
 
-        for (char r = 'a'; r <= 'z'; r++) {
-            for (int s = 8; s < s - 2 ; s--) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = row - i; j > 0; j--) {
                 System.out.print(" ");
             }
-            System.out.print(r);
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print(letter++);
+                if (letter > 'z') {
+                    letter = 'a';
+                }
+            }
             System.out.println();
-        } 
+        }
     }
 }
