d021f70b74e78701d3bf68b93ca06227d218bd51
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-15 10:01:53 +0700

    Jobsheet 9, Experiment 1.

--- Array17.java
+++ Array17.java
@@ -4,11 +4,13 @@ public class Array17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] num = new int[4];
-        int i;
-        for (i = 0; i < 4; i++) {
-            num[i]=input.nextInt();
-            
-        }
-        System.out.println(num[i]);
+        num[0]=5;
+        num[1]=12;
+        num[2]=7;
+        num[3]=20;
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
     }
 }
